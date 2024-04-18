174dac956f5df19fb899c0bbd6a1b6bad0e0cc6e
Martin Lei <martin.lei@seitenbau.com>
2024-04-18 09:01:21 +0200

    git init

---
+++ src/main/java/de/nativehint/HintsgeneratorApplication.java
@@ -0,0 +1,13 @@
+package de.nativehint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HintsgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HintsgeneratorApplication.class, args);
+	}
+
+}
