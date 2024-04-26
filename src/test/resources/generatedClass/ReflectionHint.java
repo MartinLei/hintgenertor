a3e304cc0938e0d4fad69d516acc321806b22d4e
Martin Lei <martin.lei@seitenbau.com>
2024-04-26 16:24:18 +0200

    add create from single input file

--- src/test/resources/generatedClass/ReflectionHint.java
+++ src/test/resources/generatedClass/ReflectionHint.java
@@ -3,15 +3,16 @@ package de.test;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 
-@SuppressWarnings("checkstyle:LineLength")
+@SuppressWarnings({"checkstyle:LineLength", "checkstyle:MethodLength"})
 public class GeneratedMissingNativeServiceHint implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         hints.reflection()
-            .registerType(de.dummy.AClass.java.class, MemberCategory.values())
-            .registerType(de.dummy.BClass.java.class, MemberCategory.values());
+            .registerType(TypeReference.of("de.dummy.AClass.java"), MemberCategory.values())
+            .registerType(TypeReference.of("de.dummy.BClass.java"), MemberCategory.values());
 
     }
 }
